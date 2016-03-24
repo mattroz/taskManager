ca4defd7e3aa06c92bdfe748a7550528414a63ff
matt <matveyrozanov@gmail.com>
2016-03-24 17:47:48 +0300

    Customize tasks table

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,23 @@
 module TasksHelper
+
+	def task_priority_style(task)
+		case task.priority
+		#highest priority
+		when 1
+			style = "danger"
+		#medium priority
+		when 2
+			style = "warning"
+		#low priority
+		when 3
+			style = "success"
+		else
+			style = "active"
+		end
+	end
+
+
+	def get_task_date(task)
+		_str = task.updated_at.to_s[0..9]
+	end
 end
